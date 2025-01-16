eec3507ff17e90e8ff051b1785793cb1b683031e
이효빈 <dola258@naver.com>
2025-01-16 20:35:56 +0900

    2. 컨트롤러 생성과 MockMvc 테스트

--- src/main/java/com/hyolog/controller/PostController.java
+++ src/main/java/com/hyolog/controller/PostController.java
@@ -1,7 +1,13 @@
 package com.hyolog.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PostController {
 
-    // SSR
-    // SPA
+    @GetMapping("/posts")
+    public String get() {
+        return "Hello World";
+    }
 }
---
+++ src/test/java/com/hyolog/controller/PostControllerTest.java
@@ -0,0 +1,34 @@
+package com.hyolog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청 시 Hello World를 출력한다.")
+    void test() throws Exception {
+        // expected
+        mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World")) // "Hello World2"라고 하면 PostController.java랑 다르기 때문에 오류 발생
+                .andDo(print());
+    }
+}
