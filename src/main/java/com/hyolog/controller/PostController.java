a852ccc6850e16e71ac1d799cd7f26e55468cd04
이효빈 <dola258@naver.com>
2025-01-17 13:44:21 +0900

    3-1. POST 전송, application/x-www-form-urlencoded 테스트

--- src/main/java/com/hyolog/controller/PostController.java
+++ src/main/java/com/hyolog/controller/PostController.java
@@ -1,13 +1,22 @@
 package com.hyolog.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
-    public String get() {
+    // http Method
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+
+    // 글 등록 (POST Method)
+    @PostMapping("/posts")
+    public String post(@RequestParam String title, @RequestParam String content) {
+        //System.out.println("title={}, content={}", title, content);
+        log.info("title={}, content={}", title, content);
         return "Hello World";
     }
 }
--- src/test/java/com/hyolog/controller/PostControllerTest.java
+++ src/test/java/com/hyolog/controller/PostControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -25,8 +26,15 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청 시 Hello World를 출력한다.")
     void test() throws Exception {
+        // 글 제목
+        // 글 내용
+
         // expected
-        mockMvc.perform(get("/posts"))
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("title", "글 제목입니다")
+                        .param("content", "글 내용입니다. 하하하 ")
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World")) // "Hello World2"라고 하면 PostController.java랑 다르기 때문에 오류 발생
                 .andDo(print());
