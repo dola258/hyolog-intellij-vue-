7cbd6ec19fd6792f2f60b4c981e8a5e062abec13
이효빈 <dola258@naver.com>
2025-01-17 13:50:04 +0900

    3-2. Map 오브젝트로 파라미터 받기

--- src/main/java/com/hyolog/controller/PostController.java
+++ src/main/java/com/hyolog/controller/PostController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @Slf4j
 @RestController
 public class PostController {
@@ -14,9 +16,9 @@ public class PostController {
 
     // 글 등록 (POST Method)
     @PostMapping("/posts")
-    public String post(@RequestParam String title, @RequestParam String content) {
+    public String post(@RequestParam Map<String, String> params) {
         //System.out.println("title={}, content={}", title, content);
-        log.info("title={}, content={}", title, content);
+        log.info("params: {}", params);
         return "Hello World";
     }
 }
